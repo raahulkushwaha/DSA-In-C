c1aa32f0346939fad1992b17c06db878ba67a16d
Raahul Kushwaha <kushwaharaahul96@gmail.com>
2026-07-12 22:15:42 +0530

    feat: implement Push, Pop, and Peek functions for stack

--- Stack/array-implementation.c
+++ Stack/array-implementation.c
@@ -24,6 +24,43 @@ int IsEmpty(struct stack *s)
     return s->top == -1;
 }
 
+void Push(struct stack *s, int data)
+{
+    if (IsFull(s))
+    {
+        printf("Stack Overflow! %d cannot be pushed\n", data);
+        return;
+    }
+
+    s->top++;
+    s->arr[s->top] = data;
+    printf("Pushed %d onto the stack\n", data);
+}
+
+int Pop(struct stack *s)
+{
+    if (IsEmpty(s))
+    {
+        printf("Stack Underflow!");
+        return -1;
+    }
+
+    int popped = s->arr[s->top];
+    s->top--;
+    return popped;
+}
+
+int Peek(struct stack *s)
+{
+    if (IsEmpty(s))
+    {
+        printf("Stack is empty \n");
+        return -1;
+    }
+
+    return s->arr[s->top];
+}
+
 void Display(struct stack *s)
 {
     if (IsEmpty(s))
@@ -45,5 +82,19 @@ int main()
 {
     struct stack myStack;
     Initialize(&myStack);
+
+    Push(&myStack, 10);
+    Push(&myStack, 20);
+    Push(&myStack, 30);
+    Push(&myStack, 40);
+    Push(&myStack, 50);
+    Push(&myStack, 60);
+
+    Display(&myStack);
+
+    printf("Popped value: %d\n", Pop(&myStack));
+
+    printf("Top element (Peek): %d\n", Peek(&myStack));
+
     Display(&myStack);
 }
