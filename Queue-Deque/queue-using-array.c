e686229894b0e15238d9193a75013aeb7da2ce1d
Raahul Kushwaha <kushwaharaahul96@gmail.com>
2026-07-15 22:16:32 +0530

    fix: correct IsEmpty function logic and ensure proper queue operations in Enqueue and Dequeue

--- Queue-Deque/queue-using-array.c
+++ Queue-Deque/queue-using-array.c
@@ -18,7 +18,7 @@ void Initialize(struct queue *q)
 
 int IsEmpty(struct queue *q)
 {
-    return (q->front == -1 || q->rear == -1);
+    return (q->front == -1 || q->front > q->rear);
 }
 
 int IsFull(struct queue *q)
@@ -26,10 +26,56 @@ int IsFull(struct queue *q)
     return q->rear == MAX - 1;
 }
 
+void Enqueue(struct queue *q, int data)
+{
+    if (IsFull(q))
+    {
+        printf("Queue Overflow! Cannot enqueue %d\n", data);
+        return;
+    }
+
+    if (q->front == -1)
+    {
+        q->front = 0;
+    }
+
+    q->rear++;
+    q->arr[q->rear] = data;
+    printf("Enqueued %d\n", data);
+}
+
+int Dequeue(struct queue *q)
+{
+    if (IsEmpty(q))
+    {
+        printf("Queue Underflow! Nothing be dequeued\n");
+        return -1;
+    }
+
+    int dequeuedVal = q->arr[q->front];
+    q->front++;
+
+    return dequeuedVal;
+}
+
 int main()
 {
     struct queue myQueue;
     Initialize(&myQueue);
+
+    Enqueue(&myQueue, 10);
+    Enqueue(&myQueue, 20);
+    Enqueue(&myQueue, 30);
+    Enqueue(&myQueue, 40);
+    Enqueue(&myQueue, 50);
+    Enqueue(&myQueue, 50);
+
+    printf("Dequeued value: %d\n", Dequeue(&myQueue));
+    printf("Dequeued value: %d\n", Dequeue(&myQueue));
+    printf("Dequeued value: %d\n", Dequeue(&myQueue));
+    printf("Dequeued value: %d\n", Dequeue(&myQueue));
+    printf("Dequeued value: %d\n", Dequeue(&myQueue));
+    printf("Dequeued value: %d\n", Dequeue(&myQueue));
 
     return 0;
 }
